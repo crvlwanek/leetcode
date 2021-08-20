758bdb28735bd9ba8579284e50205643fdb65d2a
crvlwanek <68828227+crvlwanek@users.noreply.github.com>
2021-08-19 20:58:23 -0500

    Create maximum_product.py

---
+++ maximum_product.py
@@ -0,0 +1,22 @@
+# 1339. Maximum Product of Splitted Binary Tree
+
+def maxProduct(self, root: Optional[TreeNode]) -> int:
+         
+  def subtree_sum(node):
+    if not node:
+        return
+    subtree_sum(node.left)
+    subtree_sum(node.right)          
+    if node.left:
+        node.val += node.left.val
+    if node.right:
+        node.val += node.right.val
+
+  def largest_product(node):
+    if not node:
+        return float('-inf')
+    current = (root.val - node.val) * node.val
+    return max(largest_product(node.left), largest_product(node.right), current)
+
+  subtree_sum(root)
+  return max(largest_product(root.left), largest_product(root.right)) % (10**9 + 7)
