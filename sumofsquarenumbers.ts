a220d319ae0f7ccc75bc5dc95840e898c8da201b
crvlwanek <68828227+crvlwanek@users.noreply.github.com>
2021-08-25 08:28:21 -0500

    Create sumofsquarenumbers.ts

---
+++ sumofsquarenumbers.ts
@@ -0,0 +1,18 @@
+// Fermat's theorem
+
+// A number can be a sum of two squares if and only if
+// all of it's prime factors in the form 4k+3 occur an
+// even number of times
+function judgeSquareSum(c: number): boolean {
+  let count;
+  for (let i = 2; i * i <= c; i++) {
+    if (c % i !== 0) continue;
+    count = 0;
+    while (c % i === 0) {
+      count++;
+      c /= i;
+    }
+    if (i % 4 === 3 && count % 2 != 0) return false;
+  }
+  return c % 4 !== 3;
+};
