5373a0c22a8fc9ef6bf788134a9036ae9de96696
crvlwanek <68828227+crvlwanek@users.noreply.github.com>
2021-08-26 08:11:43 -0500

    Update preorderserialization.py

--- preorderserialization.py
+++ preorderserialization.py
@@ -1,3 +1,6 @@
+# 331. Verify Preorder Serialization of a Binary Tree
+# https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/
+
 def isValidSerialization(preorder: str) -> bool:
   stack = ["#"]
   for node in preorder.split(","):
